b44b44b748359585419ab1ec64619ddb1132de35
04Yuuri <160242301+04Yuuri@users.noreply.github.com>
2024-04-15 00:20:06 +0900

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,39 @@
+<?php
+//1. POSTデータ取得
+$name = $_POST["name"];
+$email = $_POST["email"];
+$age = $_POST["age"];
+$hearing = $_POST["hearing"];
+$naiyou = $_POST["naiyou"];
+
+
+
+//2. DB接続します
+//*** function化する！  *****************
+include("funcs.php"); //まず、include関数でfuncs.phpを読み込む.
+$pdo = db_conn();
+
+
+//３．データ登録SQL作成
+$sql="INSERT INTO `gs_an_table`(`name`, `email`, `age`, `hearing`, `naiyou`, `indate`) VALUES(:name,:email,:age,:hearing,:naiyou,sysdate());";
+$stmt = $pdo->prepare($sql);
+
+$stmt->bindValue(':name',  $name ,PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':email', $email, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':age', $age, PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':hearing', $hearing, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':naiyou', $naiyou, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute();
+
+
+//４．データ登録処理後
+if($status==false){
+    //*** function化する！*****************
+    $error = $stmt->errorInfo();
+    exit("SQLError:".$error[2]);
+}else{
+    //*** function化する！*****************
+    header("Location: select.php");
+    exit();
+}
+?>
---
+++ select.php
@@ -0,0 +1,74 @@
+<?php
+//【重要】
+//insert.phpを修正（関数化）してからselect.phpを開く！！
+include("funcs.php");
+$pdo = db_conn();
+
+//２．データ登録SQL作成
+$sql = "SELECT * FROM gs_an_table";
+$stmt = $pdo->prepare($sql);
+$status = $stmt->execute();
+
+//３．データ表示
+$values = "";
+if($status==false) {
+  sql_error($stmt);
+}
+
+//全データ取得
+$values =  $stmt->fetchAll(PDO::FETCH_ASSOC); //PDO::FETCH_ASSOC[カラム名のみで取得できるモード]
+$json = json_encode($values,JSON_UNESCAPED_UNICODE);
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Bridge Heartコミュニティ</title>
+<link rel="stylesheet" href="css/range.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body id="main">
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+      <a class="navbar-brand" href="index.php">会員登録</a>
+      </div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+
+<!-- Main[Start] -->
+<div>
+    <div class="container jumbotron"></div>
+
+    <?php foreach($values as $values){ ?>
+      <p><?=$values["id"]?></p>
+      <p><?=$values["name"]?></p>
+      <p><?=$values["email"]?></p>
+      <p><?=$values["age"]?></p>
+      <p><?=$values["hearing"]?></p>
+      <p><?=$values["naiyou"]?></p>
+      <a href="detail.php?id=<?= $values['id']; ?>">更新</a>
+      <a href="delete.php?id=<?= $values['id']; ?>">削除</a>
+
+
+    <?php }?>
+
+</div>
+<!-- Main[End] -->
+
+<script>
+  const a = '<?php echo $json; ?>';
+  console.log(JSON.parse(a));
+</script>
+</body>
+</html>
